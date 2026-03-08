4bae2a6d798bfe4536011eb7ba6e09d5ae9945b9
Tirtha Maiti <tirtha.maiti.2006@zohomail.in>
2026-03-08 07:48:19 +0530

    Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -1,17 +1,17 @@
-//Calculate the factorial of a number given by user.
-#include<stdio.h>
-
-int fact(int n);
-
-int main() {
-    printf("factorial is: %d", fact(6));
-    return 0;
-}
+// Calculate the factorial of a number given by user.
+#include <stdio.h>
 
 int fact(int n) {
-    if(n == 0) {
-        return 1;
-    }
-    int factNm1 = fact(n-1);
-    int factN = factNm1 * n;
+    if (n == 0) return 1; //Base Case
+
+    int factNm = n * fact(n - 1); //Recurtion
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("factorial of %d: %d", n, fact(n)); //Call by Value
+    
+    return 0;
 }
